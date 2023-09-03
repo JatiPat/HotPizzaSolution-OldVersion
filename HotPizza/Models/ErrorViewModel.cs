18619f137af88c3587e353980399976afe00ccc4
Jatin Patel <110480554+JatiPat@users.noreply.github.com>
2023-09-03 11:36:14 -0400

    Add project files.

---
+++ HotPizza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotPizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
