2358c4a6c9fafa7d8ed225b6757be89ae142bbed
Jatin Patel <110480554+JatiPat@users.noreply.github.com>
2023-11-03 15:55:13 -0400

    Created Delete Post method

--- HotPizza/Controllers/StyleController.cs
+++ HotPizza/Controllers/StyleController.cs
@@ -27,7 +27,7 @@ namespace HotPizza.Controllers
         public IActionResult Create(Style newStyle)
         {
             var dupCheck = _context.Styles.Find(newStyle.DisplayOrder);
-            if (dupCheck != null) //stops users from adding same display order numbers
+            if (dupCheck is not null) //stops users from adding same display order numbers
             { //this counts as server-side validation
                 ModelState.AddModelError("displayOrder", "No duplicates for display order");
             }
@@ -45,7 +45,7 @@ namespace HotPizza.Controllers
 
         public IActionResult Edit(int? id) //Can take in id of Style that's nullable
         {
-            if(id==null || id == 0)
+            if(id is null || id == 0) //is null is better ==, and can also say //is not
             {
                 return NotFound();
             }
@@ -66,11 +66,46 @@ namespace HotPizza.Controllers
         {
             if (editStyle != null && ModelState.IsValid) 
             {
-                _context.Styles.Update(editStyle);
+                _context.Styles.Update(editStyle); //only thing needed here is to update the object and save changes
                 _context.SaveChanges();
             }
 
             return RedirectToAction("Index", "Style");
         }
+
+
+        public IActionResult Delete(int? id) //Can take in id of Style that's nullable
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Style? styleFromDb = _context.Styles.Find(id); //find can only look for the primary key
+            //Style? styleFromDb1 = _context.Styles.FirstOrDefault(u => u.Id == id); //firstordeafault using LINQ and can search for other varaibles 
+            //Style? styleFromDb2 = _context.Styles.Where(u => u.Id == id).FirstOrDefault(); //Where is used for calculation 
+            if (styleFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(styleFromDb);
+        }
+
+
+        [HttpPost, ActionName("Delete")] //telling EF Core this is the delete method even if it's called DeletePOST
+        public IActionResult DeletePOST(int? id)
+        {
+            Style? style = _context.Styles.Find(id);
+            if (style == null)
+            {
+                return BadRequest();
+            }
+            _context.Styles.Remove(style); //removing object
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Style");
+        }
     }
+
+
+}
 }
