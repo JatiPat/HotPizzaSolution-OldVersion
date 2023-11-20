c2dd71ddfd4b7c46cf1c671e2ad535d7d0b0efc4
Jatin Patel <110480554+JatiPat@users.noreply.github.com>
2023-11-20 15:51:25 -0500

    Company Model Added

---
+++ HotPizza.Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotPizza.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? PhoneNumber { get; set; }
+
+    }
+}
